ae623fa0865337b4bbae7dc63ff5702773eafb12
MrBacon470 <72811949+MrBacon470@users.noreply.github.com>
2021-03-12 14:02:04 -0600

    Fix????

--- script.js
+++ script.js
@@ -5,14 +5,14 @@ let game = {}
 
 function tick(diff) {
   // your tick things here
-  game.watts = game.watts.add(D(1).mul(diff/1000)) // makes points go up bvy 1/s
+  game.points = game.points.add(D(1).mul(diff/1000)) // makes points go up bvy 1/s
 }
 
 var app = Vue.createApp({
   data() {
     return {
       lastTick: Date.now(), // add your vars to this object, access them with game.yourVariable
-      watts: D(0)
+      points: D(0)
     }
   },
 })
