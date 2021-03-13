2296ed9fe0ec3566b754a4124f5cd14373a9fbb1
MrBacon470 <72811949+MrBacon470@users.noreply.github.com>
2021-03-13 11:43:54 -0600

    Update main.js

--- main.js
+++ main.js
@@ -26,7 +26,7 @@ function powerClick(number) {
 function purchaseGenerator(number) {
 
     for(let i = 0; i < 8; i++)
-        genCosts[i] = (genBaseCosts[i] * Math.pow(2, genLevels[i]));
+        genCosts[i] = (genBaseCosts[i] * Math.pow(1.15, genLevels[i]));
 
     if(watts >= genCosts[number]) {
         genLevels[number]++;
@@ -48,8 +48,8 @@ function purchaseGenerator(number) {
 }
 
 window.setInterval(function() {
-    powerClick(genLevels[0])
-    powerClick(10 * genLevels[1])
+    watts += genLevels[0];
+    watts += 10 * genLevels[1];
 
     document.getElementById("watts").innerHTML = `Watts: ${watts.toFixed(2)}`
 
