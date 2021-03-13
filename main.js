af676b6aa30e35788d8006bd920d88999632434f
MrBacon470 <72811949+MrBacon470@users.noreply.github.com>
2021-03-13 11:20:43 -0600

    Dark Mode

--- main.js
+++ main.js
@@ -16,6 +16,7 @@ for(let i = 0; i < 8; i++)
     genCosts[i] = Math.floor(genBaseCosts[i] * Math.pow(1.1, genLevels[i]));
 
 document.getElementById("genCost1").innerHTML = `Cost: ${genCosts[0]}`
+document.getElementById("genCost2").innerHTML = `Cost: ${genCosts[1]}`
 
 function powerClick(number) {
     watts += number
@@ -25,18 +26,30 @@ function powerClick(number) {
 function purchaseGenerator(number) {
 
     for(let i = 0; i < 8; i++)
-        genCosts[i] = Math.floor(genBaseCosts[i] * Math.pow(1.25, genLevels[i]));
+        genCosts[i] = (genBaseCosts[i] * Math.pow(2, genLevels[i]));
 
     if(watts >= genCosts[number]) {
         genLevels[number]++;
         watts -= genCosts[number];
-        document.getElementById("genCost1").innerHTML = `Cost: ${genCosts[0]}`
-        document.getElementById("gen1").innerHTML = `Manual Generators: ${genLevels[0]}`
+        for(let i = 0; i < 8; i++)
+            genCosts[i] = (genBaseCosts[i] * Math.pow(2, genLevels[i]));
+        switch(number) {
+            case 0:
+                document.getElementById("genCost1").innerHTML = `Cost: ${genCosts[0]}`
+                document.getElementById("gen1").innerHTML = `Manual Generators: ${genLevels[0]}`
+                break
+            case 1:
+                document.getElementById("genCost2").innerHTML = `Cost: ${genCosts[1]}`
+                document.getElementById("gen2").innerHTML = `Wood Burners: ${genLevels[1]}`
+                break
+        }
+
     }
 }
 
 window.setInterval(function() {
     powerClick(genLevels[0])
+    powerClick(10 * genLevels[1])
 
     document.getElementById("watts").innerHTML = `Watts: ${watts}`
 
