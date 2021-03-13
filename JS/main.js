1b98e505a8511f0fe0ed82695c154404a7d75b00
MrBacon470 <72811949+MrBacon470@users.noreply.github.com>
2021-03-13 15:46:46 -0600

    Created Scientific Notation

--- JS/main.js
+++ JS/main.js
@@ -5,8 +5,8 @@ for(let i = 0; i < 8; i++)
     genCosts[i] = (genBaseCosts[i] * Math.pow(1.1, genLevels[i]));
 
 for(let i = 0; i < generatorInfo.length; i++) {
-    document.getElementById(`${generatorInfo[i].costID}`).innerHTML = `Cost: ${genCosts[i].toFixed(2)}`
-    document.getElementById(`${generatorInfo[i].genID}`).innerHTML = `${generatorInfo[i].name}: ${genLevels[i].toFixed(2)}`
+    document.getElementById(`${generatorInfo[i].costID}`).innerHTML = `Cost: ${Notate(genCosts[i])}`
+    document.getElementById(`${generatorInfo[i].genID}`).innerHTML = `${generatorInfo[i].name}: ${Notate(genLevels[i])}`
 }
 
 function powerClick(number) {
@@ -26,8 +26,8 @@ function purchaseGenerator(number) {
             genCosts[i] = (genBaseCosts[i] * Math.pow(1.15, genLevels[i]));
 
         for(let i = 0; i < generatorInfo.length; i++) {
-            document.getElementById(`${generatorInfo[i].costID}`).innerHTML = `Cost: ${genCosts[i].toFixed(2)}`
-            document.getElementById(`${generatorInfo[i].genID}`).innerHTML = `${generatorInfo[i].name}: ${genLevels[i].toFixed(2)}`
+            document.getElementById(`${generatorInfo[i].costID}`).innerHTML = `Cost: ${Notate(genCosts[i])}`
+            document.getElementById(`${generatorInfo[i].genID}`).innerHTML = `${generatorInfo[i].name}: ${Notate(genLevels[i])}`
         }
 
     }
@@ -43,12 +43,23 @@ window.setInterval(function() {
     watts += 1e6 * genLevels[6];
     watts += 1e7 * genLevels[7];
 
-    document.getElementById("watts").innerHTML = `Watts: ${watts.toFixed(2)}`
+    document.getElementById("watts").innerHTML = `Watts: ${Notate(watts)}`
 }, 100)
 
 window.setInterval(function () {
 
     localStorage.setItem("data", btoa(JSON.stringify(data)));
 }, 1000)
+
+function Notate(x) {
+    let exponent = Math.floor(Math.log10(Math.abs(x)));
+    let mantissa = x / Math.pow(10, exponent);
+    if(x >= 1e3)
+        return `${mantissa.toFixed(2)}e${exponent}`
+    else if(x < 1e3)
+        return `${x.toFixed(2)}`;
+    else if(x == 0)
+        return '0';
+}
 
 
